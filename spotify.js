90ad5edba92af6846dcb1846993a4d2929a43921
aroy99 <aaron.roy99@yahoo.com>
2019-03-09 17:46:44 -0500

    Initial Commit

---
+++ spotify.js
@@ -0,0 +1,13 @@
+var gemoji = require('gemoji');
+
+console.log(gemoji.unicode['🐶']);
+
+
+function parse() {
+	document.getElementsByName("text");
+	
+	var i;
+	for(i=0; i < string.length; i++){
+		console.log(gemoji.unicode[string.charAt(i)].names);
+	}
+}
